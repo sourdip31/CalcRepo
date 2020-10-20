50cd94652a8b22b27f8af728d74486ccc66474ae
sourdip31 <72907752+sourdip31@users.noreply.github.com>
2020-10-20 11:19:57 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -4,7 +4,9 @@ class Calc {
         
       int num1 = 5, num2 = 15, sum;
       sum = num1 + num2;
+      prod = num1 * num2 ;
 
       System.out.println("Sum of these numbers: "+sum);
+      System.out.println("Product of these numbers: "+prod);
    }
 }
